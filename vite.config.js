91aa4bf25804fd91bfa36e031f2d31dca9214561
vijay-s2 <vijays2418@gmail.com>
2026-06-11 12:59:20 +0530

    update  the pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // or '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Resume-Portfolio-Builder-/', // 👈 ADD THIS LINE WITH YOUR REPO NAME
+  base: '/AI-Resume-Portfolio-Builder-/'
 })
